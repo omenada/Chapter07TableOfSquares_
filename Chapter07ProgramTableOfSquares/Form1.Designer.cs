8e87a3aab19bcdd3d89fc7d60109ea3c797fb013
Daniela Omena <rdaniglad@hotmail.com>
2019-05-27 20:18:38 -0400

    commit1

---
+++ Chapter07ProgramTableOfSquares/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Chapter07ProgramTableOfSquares
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.txtEnd = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lstOutput = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(46, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Starting Label Value:  ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(46, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ending Table Value";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(46, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Number ";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Location = new System.Drawing.Point(201, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Number Squared";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtStart
+            // 
+            this.txtStart.Location = new System.Drawing.Point(161, 25);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(160, 20);
+            this.txtStart.TabIndex = 4;
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Location = new System.Drawing.Point(161, 53);
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(160, 20);
+            this.txtEnd.TabIndex = 5;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(46, 96);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(147, 95);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(246, 94);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 8;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // lstOutput
+            // 
+            this.lstOutput.FormattingEnabled = true;
+            this.lstOutput.Location = new System.Drawing.Point(46, 179);
+            this.lstOutput.Name = "lstOutput";
+            this.lstOutput.ScrollAlwaysVisible = true;
+            this.lstOutput.Size = new System.Drawing.Size(275, 238);
+            this.lstOutput.TabIndex = 9;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 448);
+            this.Controls.Add(this.lstOutput);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtEnd);
+            this.Controls.Add(this.txtStart);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Text = "Table Of Squares ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.TextBox txtEnd;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.ListBox lstOutput;
+    }
+}
+
---
+++ Chapter07ProgramTableOfSquares/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter07ProgramTableOfSquares
+{
+    public partial class frmMain : Form
+    {
+        
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {   //first version of the program 
+            bool flag;
+            int i;
+            int start;
+            int end;
+            string buff;
+            int j;
+            int square;
+            int nextOddInteger;
+            //================ Gather inputs ======================
+            //Convert start from text to int
+
+            flag = int.TryParse(txtStart.Text, out start);
+            if (flag == false)
+            {
+
+                MessageBox.Show("Numeric data only", "Input Error");
+                txtStart.Focus();
+                return;
+            }
+
+            // Convert end from text to int
+            flag = int.TryParse(txtEnd.Text, out end);
+            if (flag == false)
+            {
+
+                MessageBox.Show("Numeric data only", "Input Error");
+                txtEnd.Focus();
+                return;
+            }
+
+            if (start >= end) // Reasonable values?
+            {
+                MessageBox.Show("Start greater than end.", "Input Error");
+                txtStart.Focus();
+                return;
+            }
+
+            ////================= Process and Display ============== example first for 
+
+            //for (i = start; i <= end; i++)
+            //{
+            //    buff = string.Format("{0,5}                          {1,20}", i, i * i);
+            //    lstOutput.Items.Add(buff);
+            //}
+
+            ////================= Process and Display ============== example for 
+
+            //for (i = start; i <= end; i++)
+            //{
+            //    nextOddInteger = 1; // Set first odd integer
+            //    square = 0; // Always start with square = 0
+            //    for (j = 0; j < i; j++) // Nested j loop
+            //    {
+            //        square += nextOddInteger; // Sum the odd integer
+            //        nextOddInteger += 2; // Set the next odd integer
+            //    }
+
+            //    buff = string.Format("{0, 5}        {1, 20}", i, square);
+            //    lstOutput.Items.Add(buff);
+            //}
+
+            ////================= Process and Display ============== example while 
+            i = start; // Initialize loop counter: condition 1
+            while (i <= end) // Another iteration: condition 2
+            {
+                buff = string.Format("{0, 5}{1, 20}", i, i * i);
+                lstOutput.Items.Add(buff);
+                i++; // Change state of loop: condition 3
+            }
+        }
+
+
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            txtStart.Clear();
+            txtEnd.Clear();
+            lstOutput.Items.Clear();
+        }
+    }
+}
